70e49fc85024468a787062a9cc91d95079d35507
Yuriy Volkov <sir.volkov@outlook.com>
2022-09-28 19:55:25 +0300

    dp-project | commit4

--- frn.py
+++ frn.py
@@ -1,6 +1,6 @@
 # for ITEA Programming Basics Course
 # diploma project
-# created by Yuriy Volkov 
+# created by Yuriy Volkov
 # os ---> https://docs.python.org/3/library/os.html
 # pathlib ---> https://stackabuse.com/python-list-files-in-a-directory/
 
@@ -13,9 +13,9 @@ work_folder = pathlib.Path(path)
 
 
 def menu():
-  global work_folder
+    global work_folder
 
-  cli_header = """
+    cli_header = """
     +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # welcome to small python CLI application to rename files
                               ------------------------------
@@ -40,90 +40,92 @@ def menu():
     =========================================================
 """
 
-  print(cli_header)
-  input = ""
+    print(cli_header)
+    input = ""
 
-  while (input != "q"):
-    input = get_input()
+    while (input != "q"):
+        input = get_input()
 
-    if input == "cwd":
-      print(" ")
-      print_dir()
-
-    if input == "ls":
-      for filename in work_folder.iterdir():
-        print(" ")
-        print(filename)  
-
-    if input[0:3] == "rnm":
-      arr = input.split(" ")
-           
-      if (not arr[1]) or (not arr[2]):
-        print(" ") 
-        print("._error! invalid input")
-        continue
-      else:  
-        if arr[1] and arr[2] and os.path.isfile(arr[1]):
-          print(" ")
-          print(">_ you're gonna rename: " + arr[1] + " to " + arr[2])
-          print(" ")
-          print(">_ it will impact the next file: ")
-          print(" ")
-          for filename in work_folder.iterdir():
-            if arr[1] in str(filename):
-              print(filename)
-          print(" ") 
-          print(">_ do you want to continue? [Y/n]")
-          check_out = get_input()
-          if (check_out == "n"):
+        if input == "cwd":
             print(" ")
-            print("._renaming cancelled!")
-            continue          
-          if (check_out == "Y") or (check_out == "y"):
-            full_rename(arr[1], arr[2])
-            print(" ")
-            print("._filename changed:")
-            print(" ")
+            print_dir()
+
+        if input == "ls":
             for filename in work_folder.iterdir():
-              if arr[2] in str(filename):
-                next_name = arr[2]
-                print(next_name)
-        else:
-          print(" ")
-          print("._filename wasn't found!")
-          get_input()
-                    
-    if input[0:2] == "cd":
-      print(" ")
-      temp_folder = work_folder
-  
-      if input[3:5] == "..":
-        new_path = work_folder.parent
-        work_folder = pathlib.Path(new_path)
-      else:
-        new_path = os.path.join(work_folder, input[3:])
-        work_folder = pathlib.Path(new_path)
-        
-      print_dir()
+                print(" ")
+                print(filename)
 
-    if not os.path.isdir(work_folder):
-      print(" ")
-      print("._wrong folder!")
-      work_folder = temp_folder
+        if input[0:3] == "rnm":
+            arr = input.split(" ")
+
+            if (not arr[1]) or (not arr[2]):
+                print(" ")
+                print("._error! invalid input")
+                continue
+            else:
+                if arr[1] and arr[2] and os.path.isfile(arr[1]):
+                    print(" ")
+                    print(">_ you're gonna rename: " +
+                          arr[1] + " to " + arr[2])
+                    print(" ")
+                    print(">_ it will impact the next file: ")
+                    print(" ")
+                    for filename in work_folder.iterdir():
+                        if arr[1] in str(filename):
+                            print(filename)
+                    print(" ")
+                    print(">_ do you want to continue? [Y/n]")
+                    check_out = get_input()
+                    if (check_out == "n"):
+                        print(" ")
+                        print("._renaming cancelled!")
+                        continue
+                    if (check_out == "Y") or (check_out == "y"):
+                        full_rename(arr[1], arr[2])
+                        print(" ")
+                        print("._filename changed:")
+                        print(" ")
+                        for filename in work_folder.iterdir():
+                            if arr[2] in str(filename):
+                                next_name = arr[2]
+                                print(next_name)
+                else:
+                    print(" ")
+                    print("._filename wasn't found!")
+                    get_input()
+
+        if input[0:2] == "cd":
+            print(" ")
+            temp_folder = work_folder
+
+            if input[3:5] == "..":
+                new_path = work_folder.parent
+                work_folder = pathlib.Path(new_path)
+            else:
+                new_path = os.path.join(work_folder, input[3:])
+                work_folder = pathlib.Path(new_path)
+
+            print_dir()
+
+        if not os.path.isdir(work_folder):
+            print(" ")
+            print("._wrong folder!")
+            work_folder = temp_folder
 
 
 def print_dir():
-  print(str(work_folder))
+    print(str(work_folder))
+
 
 def get_input():
-  print("")
-  return input(">_ use the command: ")
+    print("")
+    return input(">_ use the command: ")
+
 
 def full_rename(filename1, filename2):
-  last_name = filename1
-  next_name = filename2
-  os.rename(last_name, next_name)
-
+    last_name = filename1
+    next_name = filename2
+    os.rename(last_name, next_name)
 
 
 menu()
